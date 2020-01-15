27ecb8b9a6c654e2b35985601d55ce904b5ed96a
mlazzell17@gmail.com <MicheSi>
2020-01-15 14:57:00 -0700

    got dark mode to toggle

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,17 +2,17 @@ import {useState} from 'react';
 
 const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
-        if (localStorage.getItem(key)) {
-            return JSON.parse(localStorage.getItem(key));
+        if (window.localStorage.getItem(key)) {
+            return JSON.parse(window.localStorage.getItem(key));
         } else {
-            localStorage.setItem(key, JSON.stringify(initialValue));
+            window.localStorage.setItem(key, JSON.stringify(initialValue));
             return initialValue;
         }
     });
 
-    const setValue = newValue => {
-        setStoredValue(newValue);
-        localStorage.setItem(key, JSON.stringify(newValue));
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
     };
 
     return [storedValue, setValue];
